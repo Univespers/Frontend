ac54859a818671f3dbaaa01d1af4196172bb4b3c
Eryoneta <eryoneta@gmail.com>
2025-03-19 20:03:45 -0300

    [ADD] Estrutura básica
    - (ADD) Adicionado estrutura básica
    . Uma página para cada local URL: 'login', 'signin', 'colleagues', 'profile', 'profile/edit', e 'not-found'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { ColleaguesComponent } from './pages/student_domain/colleagues/colleagues.component';
+import { ProfileComponent } from './pages/student_domain/profile/profile.component';
+import { ProfileEditComponent } from './pages/student_domain/profile-edit/profile-edit.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+
+export const routes: Routes = [
+
+    // Visitor only
+    { path: "login", component: LoginComponent },
+    { path: "signin", component: SigninComponent },
+
+    // Student only
+    { path: "colleagues", component: ColleaguesComponent },
+    { path: "profile", component: ProfileComponent },
+    { path: "profile/edit", component: ProfileEditComponent },
+
+    // All
+    { path: "not-found", component: NotFoundComponent },
+    { path: "**", redirectTo: "not-found" }
+
+];
