20c850281b4a15f9b907524c9c1b6c366f41a397
Eryoneta <eryoneta@gmail.com>
2025-03-19 22:59:15 -0300

    [ADD] Pages: Logout
    - (ADD) Página de logout, disponível apenas para 'student'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SigninComponent } from './pages/guest_domain/signin/signin.component';
 import { ColleaguesComponent } from './pages/student_domain/colleagues/colleagues.component';
 import { ProfileComponent } from './pages/student_domain/profile/profile.component';
 import { ProfileEditComponent } from './pages/student_domain/profile-edit/profile-edit.component';
+import { LogoutComponent } from './pages/student_domain/logout/logout.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 import { studentAccessGuard } from './pages/student_domain/student-access.guard';
@@ -26,6 +27,7 @@ export const routes: Routes = [
     { path: "colleagues", component: ColleaguesComponent },
     { path: "profile", component: ProfileComponent },
     { path: "profile/edit", component: ProfileEditComponent, canDeactivate: [ unsavedChangesGuard ] },
+    { path: "logout", component: LogoutComponent },
   ], canActivateChild: [ studentAccessGuard ]},
 
   // All
