f0dc3f7ac8573a7ccef581ef844153496b063ea1
Eryoneta <eryoneta@gmail.com>
2025-03-26 14:22:00 -0300

    [FIX] Components/Stepper: Limites de valores
    - (FIX) O step não deve seguir abaixo de 0 ou acima da quantidade de steps

--- src/app/components/stepper/stepper.component.ts
+++ src/app/components/stepper/stepper.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, Input, Output, EventEmitter, AfterContentInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input, Output, EventEmitter, AfterContentInit, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,25 +8,54 @@ import { CommonModule } from '@angular/common';
   templateUrl: './stepper.component.html',
   styleUrl: './stepper.component.scss'
 })
-export class StepperComponent implements AfterContentInit {
+export class StepperComponent implements AfterContentInit, OnChanges {
+  // The childs
   @ViewChild('contentContainer', { static: true }) contentContainer: ElementRef | undefined;
-  quantityOfSteps = 1;
-  @Input() currentStep = 0;
-  @Output() currentStepChange = new EventEmitter<number>();
-  stepsList:number[] = [];
+
+  // Here, "currentStep" follows some rules
+  _currentStep = 0;
+  @Input() set currentStep(nextStep: number) {
+    // Enforces limits: 0 <= currentStep < this.quantityOfSteps
+    nextStep = Math.min(nextStep, this.quantityOfSteps - 1);
+    nextStep = Math.max(nextStep, 0);
+    this._currentStep = nextStep;
+    this.currentStepChange.emit(this._currentStep);
+  }
+  get currentStep(): number {
+    return this._currentStep;
+  }
+
+  // Informs parent about "currentStep" value
+  // It needs to be async!
+  @Output() currentStepChange = new EventEmitter<number>(true);
+
+  quantityOfSteps = 0;
+  stepsList: number[] = []; // Used by the template
 
   ngAfterContentInit(): void {
+    // Creates a list for the stepper-indicator
     this.quantityOfSteps = this.contentContainer?.nativeElement.children.length;
     this.stepsList = Array.apply<number,any[],number[]>(0, Array(this.quantityOfSteps)); // For SOME REASON, this is necessary
     this.stepsList = this.stepsList.map((value, index) => index + 1);
     this.updateStepper();
   }
-  setStep(index:number) {
+  ngOnChanges(changes: SimpleChanges) {
+    // Enforces limits: 0 <= currentStep < this.quantityOfSteps
+    // It does work! But the getSet solution is preferred
+    // let nextStep = changes["currentStep"].currentValue;
+    // nextStep = Math.min(nextStep, this.quantityOfSteps - 1);
+    // nextStep = Math.max(nextStep, 0);
+    // if(this.currentStep != nextStep) {
+    //   this.currentStep = nextStep;
+    //   this.currentStepChange.emit(nextStep);
+    // }
+  }
+  setStep(index:number) { // Updates currentStep
     this.currentStep = index;
-    this.currentStepChange.emit(this.currentStep);
+    // this.currentStepChange.emit(this.currentStep);
     this.updateStepper();
   }
-  updateStepper() {
+  updateStepper() { // Updates content based on currentStep
     // Hack incoming!
     for(let i = 0; i < this.quantityOfSteps; i++) {
       if(this.contentContainer != undefined) {
