d24bc268f4df541a45545427fb35727914d1b6d4
Eryoneta <eryoneta@gmail.com>
2025-03-28 17:58:11 -0300

    [ADD] GH-Pages como builder
    - (ADD) Construtor do produto final
    . O projeto pode ser lançado como um 'GitHub Page'

--- src/app/current-status.ts
+++ src/app/current-status.ts
@@ -3,7 +3,7 @@ export class CurrentStatus {
   public static GROUP_NAME = "Univespers";
   public static PROJECT_NAME = "Univespers";
   public static CURRENT_YEAR = new Date().getFullYear();
-  public static CURRENT_VERSION = "0.0.5";
+  public static CURRENT_VERSION = "1.0.3-alpha";
 
   constructor() {}
 }
