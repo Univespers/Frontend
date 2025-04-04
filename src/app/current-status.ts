002f82a66d5f96f94141b3e5bfedd9a3439025e8
Eryoneta <eryoneta@gmail.com>
2025-04-04 17:54:10 -0300

    [UPD] Pages: Botões com cores dinâmicas
    - (UPD) Implementado mudança de cor nos botões quando hover e active, em todas as páginas

--- src/app/current-status.ts
+++ src/app/current-status.ts
@@ -3,7 +3,7 @@ export class CurrentStatus {
   public static GROUP_NAME = "Univespers";
   public static PROJECT_NAME = "Univespers";
   public static CURRENT_YEAR = new Date().getFullYear();
-  public static CURRENT_VERSION = "1.0.3-alpha";
+  public static CURRENT_VERSION = "1.1.0-alpha";
 
   constructor() {}
 }
