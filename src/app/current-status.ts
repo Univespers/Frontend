319807cdd9968b56213b382c62dc2519a8f782b0
Eryoneta <eryoneta@gmail.com>
2025-04-22 23:22:53 -0300

    [ALT] Status: Versão
    - (ALT) Alterado versão para 'v1.2.0-alpha'

--- src/app/current-status.ts
+++ src/app/current-status.ts
@@ -3,7 +3,8 @@ export class CurrentStatus {
   public static GROUP_NAME = "Univespers";
   public static PROJECT_NAME = "Univespers";
   public static CURRENT_YEAR = new Date().getFullYear();
-  public static CURRENT_VERSION = "1.1.1-alpha";
+  public static CURRENT_VERSION = "1.2.0-alpha";
 
   constructor() {}
+
 }
