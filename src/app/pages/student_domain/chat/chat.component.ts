3e4a07c43f985cd52da64bce36cdb422896c9dc6
Guilherme Duarte <guilhermeduarte.informatica@gmail.com>
2025-09-15 18:50:40 -0300

    [UPD] Melhorias na tela de chat, agora com um campo de busca (não funcional) e com layout que divide lista de contato das mensagens

--- src/app/pages/student_domain/chat/chat.component.ts
+++ src/app/pages/student_domain/chat/chat.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule, // para usar FormControl
+    MatFormFieldModule, // para <mat-form-field>
+    MatInputModule, // para matInput
+    MatAutocompleteModule, ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent {
-
+export class ChatComponent implements OnInit {
+  searchControl = new FormControl('');
+  ngOnInit(): void{}
 }
